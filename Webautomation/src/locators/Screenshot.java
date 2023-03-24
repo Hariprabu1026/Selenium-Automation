8dda9c81f242e9289e0fb38bcd59307440b8694c
Hariprabu1026 <128838708+Hariprabu1026@users.noreply.github.com>
2023-03-24 23:13:06 +0530

    First Commit from eclipse

---
+++ Webautomation/src/locators/Screenshot.java
@@ -0,0 +1,33 @@
+package locators;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Screenshot {
+	public static void main (String args[]) throws IOException {
+		
+	System.setProperty("webdriver.chrome.driver", "D:\\Automation\\chrome driver\\chromedriver_win32\\chromedriver.exe");
+	
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("--remote-allow-origins=*");
+	//options.setAcceptInsecureCerts(true);
+	options.addArguments("start-maximized");
+	//options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
+	ChromeDriver driver = new ChromeDriver(options);
+	
+	driver.get("https://www.google.com/");
+	
+	File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("D://screenshot.png"));
+	
+	
+	//Test
+	}
+}
