a827eeb5c8c3d9918130b8cf5d420a4aec1c4499
Hariprabu1026 <128838708+Hariprabu1026@users.noreply.github.com>
2023-03-26 18:23:05 +0530

    Create a Pyramid

---
+++ Webautomation/src/locators/Pyramid.java
@@ -0,0 +1,32 @@
+package locators;
+import java.util.Scanner;
+
+
+
+	public class Pyramid {
+
+	    public static void main(String[] args) {
+
+	        // Create a scanner to read the user's input
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        // Get the number of rows for the pyramid from the user
+	        System.out.print("Enter the number of rows: ");
+	        int rows = scanner.nextInt();
+
+	        // Create the pyramid
+	        for (int i = 1; i <= rows; i++) {
+	            // Print spaces before the stars
+	            for (int j = 1; j <= rows - i; j++) {
+	                System.out.print(" ");
+	            }
+	            // Print the stars
+	            for (int k = 1; k <= 2 * i - 1; k++) {
+	                System.out.print("*");
+	            }
+	            // Move to the next line
+	            System.out.println();
+	        }
+	    }
+	}
+
