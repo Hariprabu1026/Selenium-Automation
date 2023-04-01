3131dd87422e105023fa4f347d8f56a6b6b46d7d
Hariprabu1026 <128838708+Hariprabu1026@users.noreply.github.com>
2023-04-01 22:28:15 +0530

    StreamsDemo in Live application

--- Webautomation/src/locators/Pyramid.java
+++ Webautomation/src/locators/Pyramid.java
@@ -1,19 +1,17 @@
 package locators;
 import java.util.Scanner;
 
-
-
-	public class Pyramid {
+public class Pyramid {
 
 	    public static void main(String[] args) {
-
-	        // Create a scanner to read the user's input
+	    	
+	    	// Create a scanner to read the user's input
 	        Scanner scanner = new Scanner(System.in);
 	        
 	        // Get the number of rows for the pyramid from the user
 	        System.out.print("Enter the number of rows: ");
 	        int rows = scanner.nextInt();
-
+	        
 	        // Create the pyramid
 	        for (int i = 1; i <= rows; i++) {
 	            // Print spaces before the stars
---
+++ Webautomation/src/locators/StreamsDemo.java
@@ -0,0 +1,83 @@
+package locators;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class StreamsDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Automation\\chrome driver\\chromedriver_win32_111\\chromedriver.exe");
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.addArguments("start-maximized");
+		ChromeDriver driver = new ChromeDriver(options);
+
+		// driver.manage().window().maximize();
+
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		driver.findElement(By.linkText("Top Deals")).click();
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parentWindow = it.next();
+		String childWindow = it.next();
+
+		driver.switchTo().window(childWindow);
+
+		driver.findElement(By.cssSelector("span.sort-icon.sort-descending")).click();
+		
+//		for(int i=0 ; i<vegNames.size() ; i++)
+//		{
+//			String vegName = vegNames.get(i).getText();
+//			System.out.println(vegName);
+//			if(vegName.contains("Beans")) {
+//				
+//				
+//			}
+//		}
+		//List<String> originalNames = vegNames.stream().map(s -> s.getText()).collect(Collectors.toList());
+		// sorting the originalNames
+//		List<String> sortedNames = originalNames.stream().sorted().collect(Collectors.toList());
+
+		//Assert.assertTrue(originalNames.equals(sortedNames));
+		List<String> price;
+		// Get price of each vegetables
+		do {
+			List<WebElement> vegNames = driver.findElements(By.xpath("//tbody/tr/td[1]"));
+			price = vegNames.stream().filter(s -> s.getText().contains("Rice")).map(s -> getPriceVeggie(s))
+					.collect(Collectors.toList());
+
+			price.forEach(a -> System.out.println(a));
+
+			if (price.size() < 1) {
+				driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
+
+			}
+		} while (price.size() < 1);
+
+	}
+
+	private static String getPriceVeggie(WebElement s) {
+
+		String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return priceValue;
+	}
+
+}
